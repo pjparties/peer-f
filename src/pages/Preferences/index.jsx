67093cdaf90e21fb6c3959860479d17ca6ca334d
parth <parthhjuneja@gmail.com>
2024-07-13 01:23:26 +0530

    feat: Improve preference card descriptions and layout

--- src/pages/Preferences/index.jsx
+++ src/pages/Preferences/index.jsx
@@ -11,7 +11,7 @@ const PreferenceCard = ({ title, description, image, location }) => {
     <div className="py-5 px-4 text-zinc flex flex-col justify-evenly shadow-sm rounded-xl border border-secondary/20">
       <img className="w-16 h-16" src={image} />
       <h2 className="left-4 top-16 text-base font-bold w-fit leading-tight tracking-tight mt-2">{title}</h2>
-      <p className="w-44 mt-3 text-xs font-medium leading-4">{description}</p>
+      <p className="w-44 mt-3 text-xs font-medium leading-4 line-clamp-3">{description}</p>
       <Link to={"/chat/"+location}>
         <button className="mt-3 px-3 py-2 w-fit hover:scale-105 transition-all duration-200 ease-in-out bg-secondary rounded-lg justify-center items-center text-white text-xs font-semibold">
           Start Chat
@@ -25,7 +25,7 @@ const PreferenceWindow = () => {
   const Preferences = [
     {
       title: "Pair Programming",
-      description: "Connect with someone to work on a project together, share ideas and learn from each other.",
+      description: "Connect to work on a project together, share ideas and learn from each other.",
       image: pair,
       location: "1",
     },
@@ -43,7 +43,7 @@ const PreferenceWindow = () => {
     },
   ];
   return (
-    <div className="windowcontainer w-2/3 h-2/3 rounded-2xl border border-lightzinc bg-white p-12">
+    <div className="windowcontainer rounded-2xl border border-lightzinc bg-white p-10">
       <div className="content font-Inter flex flex-col justify-center items-center px-4">
         <div className="textsection flex-col justify-start gap-2 inline-flex">
           <h1 className="text-zinc text-xl font-semibold leading-8 tracking-tight">Select your preferences</h1>
@@ -65,7 +65,6 @@ const PreferenceWindow = () => {
     </div>
   );
 };
-
 
 export const Preferences = () => {
   return (
