dcd11e99e45de6f93c657f5386f0b5de90dcae50
parth <parthhjuneja@gmail.com>
2024-07-13 01:19:15 +0530

    chore: updates to card descriptions

--- src/pages/Preferences/index.jsx
+++ src/pages/Preferences/index.jsx
@@ -10,8 +10,8 @@ const PreferenceCard = ({ title, description, image, location }) => {
   return (
     <div className="py-5 px-4 text-zinc flex flex-col justify-evenly shadow-sm rounded-xl border border-secondary/20">
       <img className="w-16 h-16" src={image} />
-      <h2 className="left-4 top-16 text-base font-bold w-fit leading-tight tracking-tight">{title}</h2>
-      <p className="w-44 left-4 top-24 text-xs font-medium leading-4">{description}</p>
+      <h2 className="left-4 top-16 text-base font-bold w-fit leading-tight tracking-tight mt-2">{title}</h2>
+      <p className="w-44 mt-3 text-xs font-medium leading-4">{description}</p>
       <Link to={"/chat/"+location}>
         <button className="mt-3 px-3 py-2 w-fit hover:scale-105 transition-all duration-200 ease-in-out bg-secondary rounded-lg justify-center items-center text-white text-xs font-semibold">
           Start Chat
@@ -25,19 +25,19 @@ const PreferenceWindow = () => {
   const Preferences = [
     {
       title: "Pair Programming",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+      description: "Connect with someone to work on a project together, share ideas and learn from each other.",
       image: pair,
       location: "1",
     },
     {
       title: "Mock Interview",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+      description: "Got an interview coming up? Practice with someone who can help you prepare.",
       image: mock,
       location: "2",
     },
     {
       title: "Code Debug",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+      description: "Stuck on a bug? Get help from someone who can help you debug your code.",
       image: debug,
       location: "2",
     },
